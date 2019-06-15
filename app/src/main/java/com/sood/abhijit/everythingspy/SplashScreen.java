7d3715c85f2258d0b9dd2957b818ce1276070222
Abhijit Sood <sood232@gmail.com>
2019-06-15 19:54:30 +0530

    First commit

---
+++ app/src/main/java/com/sood/abhijit/everythingspy/SplashScreen.java
@@ -0,0 +1,42 @@
+package com.sood.abhijit.everythingspy;
+
+/**
+ * Created by Abhijit on 04-10-2017.
+ */
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
+}
